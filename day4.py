c489f2ac11c64f9ee7af3a3cad697ed01c3ba22d
VSreeHarshitha <sreeharshithavakkantham246@gmail.com>
2025-01-08 07:25:00 +0000

    day4

---
+++ day4.py
@@ -0,0 +1,16 @@
+def sum_of_evens(n):
+    if n <= 0:
+        return 0  # Explicitly return 0 for non-positive values of n
+    even_sum = sum(i for i in range(2, n + 1, 2))
+    return even_sum
+
+n = 10
+result = sum_of_evens(n)
+print(f"The sum of all even numbers between 1 and {n} is: {result}")
+                
+
+
+
+
+
+
